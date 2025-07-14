446a8d93b04d4a4490bba4ef9d2711be488a68f3
Chamodi Sandeepani <sandeepanichamodi999@gmail.com>
2025-07-14 19:55:40 +0530

    home

---
+++ script.js
@@ -0,0 +1,19 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+};
+
+var swiper = new Swiper(".home-slider", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
