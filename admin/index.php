ae38589f43c68c62e0d8f37d7a6391974833f385
Dilmin Ekanayaka <83674010+dilminekanayaka@users.noreply.github.com>
2025-08-08 23:55:42 +0530

    links updated

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login Panel</title>
+    <?php
+        require('inc/links.php');
+    ?>    
+</head>
+<body class="bg-warning">
+    
+</body>
+</html>
